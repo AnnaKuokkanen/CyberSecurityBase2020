8e5e2a9aaf90383851eef916f0c74cececbca808
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-12-25 19:02:52 +0200

    Add login view

--- eventcalendar/events/views.py
+++ eventcalendar/events/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. Welcome to your Event Calendar")
+def login(request): 
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
