a55ea07d9b51c06ea74ffa89820a82800121722d
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-12-26 05:20:19 +0200

    Add logout button, expand database schema

--- eventcalendar/events/models.py
+++ eventcalendar/events/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
+class Participant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 class Event(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
+    participants = models.ManyToManyField(Participant)
